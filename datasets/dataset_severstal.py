a5ea72df67fdd09001d3fc9399b17681bfc99551
Sandeep Kagithalagudem <sandeep.kagithalagudem@tdwilliamson.com>
2022-04-26 14:16:26 +0200

    modified code

--- datasets/dataset_severstal.py
+++ datasets/dataset_severstal.py
@@ -96,11 +96,16 @@ class Severstal_dataset(Dataset):
     def __getitem__(self, idx):
         # if self.split == "train":
         slice_name = self.sample_list[idx].strip('\n')
-        fileName = slice_name + '.jpg'
-        img = cv2.imread(os.path.join(self.data_dir, fileName))
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        img = (img - np.min(img))/(np.max(img) - np.min(img))
-        mask = self.maskGenerator.build_mask(fileName)
+        if 'dataset' in self.data_dir:
+            data_path = os.path.join(self.data_dir, slice_name+'.npz')
+            data = np.load(data_path)
+            image, label = data['image'], data['label']
+        else:
+            fileName = slice_name + '.jpg'
+            img = cv2.imread(os.path.join(self.data_dir, fileName))
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            img = (img - np.min(img))/(np.max(img) - np.min(img))
+            mask = self.maskGenerator.build_mask(fileName)
 
         sample = {'image': img, 'label': mask}
         if self.transform:
